f6808da9e5d7a73d57d40892fd6b597c0f066330
Stem Stem <stem@astanait.edu.kz>
2023-02-18 11:08:19 +0600

    Created class Book

---
+++ src/Book.java
@@ -0,0 +1,16 @@
+public class Book {
+    private static Integer nextID = 1000;
+    private Integer bookID;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author){
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+        this.bookID  = nextID++;
+    }
+
+}
+
