8102b6fde982d68b5e109dd22c189c5246e9c995
Stem Stem <stem@astanait.edu.kz>
2023-02-18 11:22:23 +0600

    Created method for putting information about book

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Book {
     private static Integer nextID = 1000;
     private Integer bookID;
@@ -35,5 +36,21 @@ public class Book {
         System.out.println();
     }
 }
-}
+public class BookInfo{
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        input.nextLine();
+        Book[] books = new Book[num];
+        String title;
+        String author;
+        for (int i = 0; i < num; i++) {
+            title = input.nextLine();
+            author = input.nextLine();
+            books[i] = new Book(title,author);
+        }
+        for (int i = 0; i < num; i++) {
+            books[i].show();
+        }
+    }
+
 
