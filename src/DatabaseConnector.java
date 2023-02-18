f5049d30fa4d3cdd1d99f5ceb1e90efacc46fd75
stagepalete <edige.mazhit.2004@gmail.com>
2023-02-18 11:17:39 +0600

    Created class DatabaseConnector and connect method to database

--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -3,5 +3,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnector {
-     //getter connection status to check
+    private static final String URL = "jdbc:mysql://127.0.0.1:3306/libraryassignment";  //Connection URL to database
+    private static final String  USERNAME = "root"; //Username to access database
+    private static final String PASSWORD = ""; //password to access database
+
+    public static Connection connect(){ //Method to connect to database
+        Connection connection = null; //Initializing connection
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); //Getting connection with getConnect()
+            
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return connection; //returning connection
+    }
+
+    //getter connection status to check
 }
