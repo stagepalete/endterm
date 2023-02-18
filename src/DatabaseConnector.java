387fc2930a1e9c2c7e9f6df13713811bf101a70e
stagepalete <edige.mazhit.2004@gmail.com>
2023-02-18 11:18:24 +0600

    Added isConnected method in DatabaseConnector

--- src/DatabaseConnector.java
+++ src/DatabaseConnector.java
@@ -6,17 +6,19 @@ public class DatabaseConnector {
     private static final String URL = "jdbc:mysql://127.0.0.1:3306/libraryassignment";  //Connection URL to database
     private static final String  USERNAME = "root"; //Username to access database
     private static final String PASSWORD = ""; //password to access database
-
+    private static boolean isConnected = false; //Connection status
     public static Connection connect(){ //Method to connect to database
         Connection connection = null; //Initializing connection
         try {
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD); //Getting connection with getConnect()
-            
+            isConnected = true; //Changing connection status to true/ By default its false
         }catch(SQLException e){
             e.printStackTrace();
         }
         return connection; //returning connection
     }
 
-    //getter connection status to check
+    public static boolean isConnected(){
+        return isConnected;
+    } //getter connection status to check
 }
