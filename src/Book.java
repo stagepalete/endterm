7465d66aa3dd9fa7c976d6af965371eaef8de6e0
ArmanJ04 <dzhansatov00@gmail.com>
2023-02-19 11:35:40 +0600

    created a constructor for Book class

--- src/Book.java
+++ src/Book.java
@@ -4,4 +4,11 @@ public class Book {
     private String author;
     private int year_of_publish;
     private String isbn;
+    public Book(int id, String name, String author, int year_of_publish, String isbn) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.year_of_publish = year_of_publish;
+        this.isbn = isbn;
+    }
 }
