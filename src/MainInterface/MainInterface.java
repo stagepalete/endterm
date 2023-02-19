d55999863c0e4b481b269f10e6654b34af8f7000
muratov030510 <221058@astanait.edu.kz>
2023-02-19 13:39:07 +0600

    Created new class Menu

--- src/MainInterface/MainInterface.java
+++ src/MainInterface/MainInterface.java
@@ -11,55 +11,18 @@ public class MainInterface implements Login, Register{
     private static User currentUser = null;
     public static void menu(){
         if(currentUser == null){
-            LoginMenu();
+            Menu.LoginMenu();
         }else if(!User.isAdmin()){
-            UserMenu();
+            Menu.UserMenu();
         }else{
-            AdminMenu();
+            Menu.AdminMenu();
         }
     }
 
 
 
 
-    public static void UserMenu(){
-        // Shows all available methods
-        ArrayList<String> menu = new ArrayList<String>();
-        menu.add("Show all available books");
-        menu.add("Pick up books");
-        menu.add("Search");
-        menu.add("Return book");
-        menu.add("MyBooks");
-        showOptions(menu);
-    }
-    public static void AdminMenu(){
-        ArrayList<String> menu = new ArrayList<String>();
-        menu.add("Add book");
-        menu.add("Delete book");
-        menu.add("Search book");
-        menu.add("Update book");
-        showOptions(menu);
-    }
-    public static void SearchMenu(){
-        //Shows searching methods
-        ArrayList<String> menu = new ArrayList<String>();
-        menu.add("Search by id");
-        menu.add("Search by name");
-        menu.add("Search by author");
-        showOptions(menu);
-    }
-    public static void LoginMenu(){
-        // Shows login options
-        ArrayList<String> menu = new ArrayList<String>();
-        menu.add("Login"); menu.add("Register");
-        showOptions(menu);
-    }
-    public static void showOptions(ArrayList<String> options){
-        int c = 1;
-        for (String elem:options){
-            System.out.printf("%d) %s%n", c++, elem);
-        }
-    }
+
     @Override
     public void Login() {
 
