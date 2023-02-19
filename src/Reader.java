d2da126af47fb99f077032ad3b52adf5ed140886
johnsukadoe <bking4234@gmail.com>
2023-02-19 11:13:17 +0600

    Created class Reader that extends from User class

---
+++ src/Reader.java
@@ -0,0 +1,9 @@
+public class Reader extends User {
+
+    public Reader(int id, String name, String lastname, String username, String password) {
+        super(id, name, lastname, username, password);
+    }
+
+    // Add any additional methods or properties specific to the Reader class here
+
+}
--- src/User.java
+++ src/User.java
@@ -17,9 +17,6 @@ public class User {
         PASSWORD = password;
     }
 
-    public void addBook(String name, String author, String date, String isbn) throws SQLException{
-        state.addBook(name, author, date, isbn);
-    }
 
     public void setState(State state){
         this.state = state;
