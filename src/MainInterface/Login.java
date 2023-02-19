2733c06edb23ae3b58d4e74dc92e5d08157ee6e4
johnsukadoe <bking4234@gmail.com>
2023-02-19 22:19:27 +0600

    Login fixed

--- src/MainInterface/Login.java
+++ src/MainInterface/Login.java
@@ -12,18 +12,18 @@ import java.util.Objects;
 
 public interface Login {
     static User Login(String username, String password) throws SQLException {
-        String query = "Select * from `users`";
+        String query = "Select * from `users` where `username` = '%s'".formatted(username);
         ResultSet resultset = DatabaseConnector.executeQueryRead(query);
         while(resultset.next()){
             if(Objects.equals(resultset.getString("username"), username) && Objects.equals(resultset.getString("password"), password)){
-                int id = resultset.getInt("id");
+                String id = resultset.getString("id");
                 String name = resultset.getString("name");
                 String lastname =resultset.getString("lastname");
                 if(Objects.equals(resultset.getString("isAdmin"), "1")){
-                    return new Admin();
+                    return new Admin(Integer.parseInt(id), name, lastname, username);
                 }else if(Objects.equals(resultset.getString("isAdmin"), "0")){
                     System.out.printf("Welcome, %s%n", name);
-                    return new Reader(id, name, lastname, username);
+                    return new Reader(Integer.parseInt(id), name, lastname, username);
                 }
             }else{
                 System.out.println("Wrong username or password!");
--- src/Users/Admin.java
+++ src/Users/Admin.java
@@ -1,4 +1,8 @@
 package Users;
 
 public class Admin extends User{
+    public Admin(int id, String name, String lastname, String username) {
+        super(id, name, lastname, username);
+    }
+
 }
