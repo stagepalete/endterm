a1129aaa88f51000dae6e62c02aa113647ae136b
ArmanJ04 <dzhansatov00@gmail.com>
2023-02-19 11:38:49 +0600

    created getters, setters and other methods for Book class

--- src/Book.java
+++ src/Book.java
@@ -11,4 +11,43 @@ public class Book {
         this.year_of_publish = year_of_publish;
         this.isbn = isbn;
     }
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYear_of_publish() {
+        return year_of_publish;
+    }
+
+    public void setYear_of_publish(int year_of_publish) {
+        this.year_of_publish = year_of_publish;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
